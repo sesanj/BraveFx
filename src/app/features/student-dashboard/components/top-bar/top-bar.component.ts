5ae48e583aa3ef21b8af931cd54fdd5246cb42cb
Sesan Joel <popoolasesan4@gmail.com>
2025-11-05 21:42:15 -0500

    Student Dashboard, Optimised For All Devices

--- src/app/features/student-dashboard/components/top-bar/top-bar.component.ts
+++ src/app/features/student-dashboard/components/top-bar/top-bar.component.ts
@@ -2,11 +2,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   LucideAngularModule,
-  Bell,
   User,
   Settings,
   LogOut,
+  Moon,
+  Sun,
 } from 'lucide-angular';
+import { ThemeService } from '../../../../core/services/theme.service';
 
 export interface UserData {
   name: string;
@@ -27,12 +29,15 @@ export class TopBarComponent {
   @Output() logoutClick = new EventEmitter<void>();
 
   // Icons
-  Bell = Bell;
   User = User;
   Settings = Settings;
   LogOut = LogOut;
+  Moon = Moon;
+  Sun = Sun;
 
   isUserMenuOpen = false;
+
+  constructor(public theme: ThemeService) {}
 
   toggleUserMenu(): void {
     this.isUserMenuOpen = !this.isUserMenuOpen;
@@ -45,5 +50,9 @@ export class TopBarComponent {
 
   onLogout(): void {
     this.logoutClick.emit();
+  }
+
+  toggleTheme(): void {
+    this.theme.toggleTheme();
   }
 }
